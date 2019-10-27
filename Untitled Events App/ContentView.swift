c8e60dff2cc2712c8c720615becf2e00da47e5e3
Louis Tsai <louis993546@gmail.com>
2019-10-27 23:09:42 +0100

    Initial Commit

---
+++ Untitled Events App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Untitled Events App
+//
+//  Created by Louis Tsai on 10/27/19.
+//  Copyright © 2019 Louis Tsai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
