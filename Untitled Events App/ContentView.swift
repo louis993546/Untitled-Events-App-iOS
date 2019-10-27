3b92b31e794466a4786ea7f53a121e2b175bfa02
Louis Tsai <louis993546@gmail.com>
2019-10-27 23:14:27 +0100

    Initial commit

--- Untitled Events App/ContentView.swift
+++ Untitled Events App/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        TabView(selection: /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1")
+                .tabItem { Text("Tab Label 1") }
+                .tag(1)
+            Text("Tab Content 2")
+                .tabItem { Text("Tab Label 2") }
+                .tag(2)
+        }
     }
 }
 
